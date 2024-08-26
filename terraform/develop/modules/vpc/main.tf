3843355c48106bf9dfedc942317ae50cef9670f0
fingersdanny <kbry12@naver.com>
2024-08-26 18:59:12 +0900

    feat : 서브넷에 가용영역 추가

--- terraform/develop/modules/vpc/main.tf
+++ terraform/develop/modules/vpc/main.tf
@@ -6,6 +6,7 @@ resource "aws_subnet" "public_subnets" {
   count = length(var.public_subnet_cidrs)
   vpc_id = aws_vpc.main.id
   cidr_block = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
 
   tags = merge(var.tags, {
     Name = "Public Subnet ${count.index + 1}"
@@ -16,6 +17,7 @@ resource "aws_subnet" "private_subnets" {
   count = length(var.private_subnet_cidrs)
   vpc_id = aws_vpc.main.id
   cidr_block = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
 
   tags = merge(var.tags, {
     Name = "Private Subnet ${count.index + 1}"
