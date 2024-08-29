94bdde05de41d209dce9cee6f3ab74051351675b
fingersdanny <kbry12@naver.com>
2024-08-29 17:46:37 +0900

    feat : tag에 Date 추가

--- terraform/develop/modules/vpc/main.tf
+++ terraform/develop/modules/vpc/main.tf
@@ -10,77 +10,84 @@ resource "aws_subnet" "public_subnets" {
 
   tags = merge(var.tags, {
     Name = "Public Subnet ${count.index + 1}"
+    Date = timestamp()
   })
 }
 
-resource "aws_subnet" "private_subnets" {
-  count = length(var.private_subnet_cidrs)
-  vpc_id = aws_vpc.main.id
-  cidr_block = element(var.private_subnet_cidrs, count.index)
-  availability_zone = element(var.azs, count.index)
+    resource "aws_subnet" "private_subnets" {
+      count = length(var.private_subnet_cidrs)
+      vpc_id = aws_vpc.main.id
+      cidr_block = element(var.private_subnet_cidrs, count.index)
+      availability_zone = element(var.azs, count.index)
 
-  tags = merge(var.tags, {
-    Name = "Private Subnet ${count.index + 1}"
-  })
-}
+      tags = merge(var.tags, {
+        Name = "Private Subnet ${count.index + 1}"
+        Date = timestamp()
+      })
+    }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+    resource "aws_internet_gateway" "igw" {
+      vpc_id = aws_vpc.main.id
 
-  tags = merge(var.tags, {
-    Name = "VPC Internet Gateway"
-  })
-}
+      tags = merge(var.tags, {
+        Name = "VPC Internet Gateway"
+        Date = timestamp()
+      })
+    }
 
-resource "aws_route_table" "public_routing_table" {
-  vpc_id = aws_vpc.main.id
+    resource "aws_route_table" "public_routing_table" {
+      vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+      route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
+      }
 
-  tags = merge(var.tags, {
-    Name = "Public Routing Table"
-  })
-}
+      tags = merge(var.tags, {
+        Name = "Public Routing Table"
+        Date = timestamp()
+      })
+    }
 
-resource "aws_route_table" "private_routing_table" {
-  vpc_id = aws_vpc.main.id
+    resource "aws_route_table" "private_routing_table" {
+      vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
-  }
+      route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_nat_gateway.nat_gw.id
+      }
 
-  tags = merge(var.tags, {
-    Name = "Private Routing Table"
-  })
-}
+      tags = merge(var.tags, {
+        Name = "Private Routing Table"
+        Date = timestamp()
+      })
+    }
 
-resource "aws_eip" "nat_eip" {
-  tags = merge(var.tags, {
-    Name = "NAT Gateway EIP"
-  })
-}
+    resource "aws_eip" "nat_eip" {
+      tags = merge(var.tags, {
+        Name = "NAT Gateway EIP"
+        Date = timestamp()
+      })
+    }
 
-resource "aws_nat_gateway" "nat_gw" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id = aws_subnet.public_subnets[0].id
+    resource "aws_nat_gateway" "nat_gw" {
+      allocation_id = aws_eip.nat_eip.id
+      subnet_id     = aws_subnet.public_subnets[0].id
 
-  tags = merge(var.tags, {
-    Name = "NAT Gateway"
-  })
-}
+      tags = merge(var.tags, {
+        Name = "NAT Gateway"
+        Date = timestamp()
+      })
+    }
 
-resource "aws_route_table_association" "private_subnet_association" {
-  count = length(var.private_subnet_cidrs)
-  subnet_id = aws_subnet.private_subnets[count.index].id
-  route_table_id = aws_route_table.private_routing_table.id
-}
+    resource "aws_route_table_association" "private_subnet_association" {
+      count = length(var.private_subnet_cidrs)
+      subnet_id      = aws_subnet.private_subnets[count.index].id
+      route_table_id = aws_route_table.private_routing_table.id
+    }
 
-resource "aws_route_table_association" "public_subnet_association" {
-  count = length(var.public_subnet_cidrs)
-  subnet_id = aws_subnet.public_subnets[count.index].id
-  route_table_id = aws_route_table.public_routing_table.id
-}
+    resource "aws_route_table_association" "public_subnet_association" {
+      count = length(var.public_subnet_cidrs)
+      subnet_id      = aws_subnet.public_subnets[count.index].id
+      route_table_id = aws_route_table.public_routing_table.id
+    }
